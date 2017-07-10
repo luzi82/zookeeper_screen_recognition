ceff3facd2e9f29636a73e247a4c3bcda7f3196d
luzi82 <luzi82@gmail.com>
2017-07-10 12:41:43 +0800

    update check_state.py

---
+++ _util.py
@@ -0,0 +1,14 @@
+import os
+
+def get_label_state_list():
+    label_state_path = os.path.join('label','state')
+    label_name_list = os.listdir(label_state_path)
+    label_name_list = filter(lambda v:os.path.isfile(os.path.join(label_state_path,v)),label_name_list)
+    label_name_list = filter(lambda v:v.endswith('.txt'),label_name_list)
+    label_name_list = [ i[:-4] for i in label_name_list]
+    label_name_list = sorted(label_name_list)
+    return label_name_list
+
+def makedirs(path):
+    if not os.path.isdir(path):
+        os.makedirs(path)
--- check_state.py
+++ check_state.py
@@ -1,18 +1,26 @@
 import os
+import _util
 
 if __name__ == '__main__':
     import argparse
     import shutil
 
     parser = argparse.ArgumentParser(description='state')
-    parser.add_argument('state', help='state')
+    parser.add_argument('state', nargs='?', help='state')
     args = parser.parse_args()
+
+    if args.state != None:
+        state_list = [state]
+    else:
+        state_list = _util.get_label_state_list()
 
     shutil.rmtree('check',ignore_errors=True)
     os.makedirs('check')
-    with open(os.path.join('label','state','{}.txt'.format(args.state)),'r') as fin:
-        file_list = fin.readlines()
-    file_list = [ i.strip() for i in file_list ]
-    for f in file_list:
-        _, t = os.path.split(f)
-        shutil.copyfile( f, os.path.join('check',t) )
+    for state in state_list:
+        with open(os.path.join('label','state','{}.txt'.format(state)),'r') as fin:
+            file_list = fin.readlines()
+        file_list = [ i.strip() for i in file_list ]
+        for f in file_list:
+            _, t = os.path.split(f)
+            _util.makedirs(os.path.join('check',state))
+            shutil.copyfile( f, os.path.join('check',state,t) )
