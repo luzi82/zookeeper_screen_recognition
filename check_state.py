a54f8254f1270157a89d4faac6e45cf00b220481
Luzi82 <luzi82@gmail.com>
2017-07-10 20:58:59 +0800

    check_state.py output output folder

--- check_state.py
+++ check_state.py
@@ -14,13 +14,13 @@ if __name__ == '__main__':
     else:
         state_list = _util.get_label_state_list()
 
-    shutil.rmtree('check',ignore_errors=True)
-    os.makedirs('check')
+    shutil.rmtree('output',ignore_errors=True)
+    os.makedirs('output')
     for state in state_list:
         with open(os.path.join('label','state','{}.txt'.format(state)),'r') as fin:
             file_list = fin.readlines()
         file_list = [ i.strip() for i in file_list ]
         for f in file_list:
             _, t = os.path.split(f)
-            _util.makedirs(os.path.join('check',state))
-            shutil.copyfile( f, os.path.join('check',state,t) )
+            _util.makedirs(os.path.join('output',state))
+            shutil.copyfile( f, os.path.join('output',state,t) )
