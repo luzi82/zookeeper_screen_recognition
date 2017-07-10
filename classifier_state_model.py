4a3bd19ef1a14ac801e0df663e0a0b7bb91a5ad9
luzi82 <luzi82@gmail.com>
2017-07-10 10:59:22 +0800

    remove l2, add normalization

--- classifier_state_model.py
+++ classifier_state_model.py
@@ -9,60 +9,36 @@ PHI = (1+5**0.5)/2
 
 def create_model(label_count):
     model = Sequential()
-    model.add(Conv2D(
-        filters=32, kernel_size=3, padding='valid', activation='elu', input_shape=(HEIGHT,WIDTH,5),
-        kernel_regularizer = regularizers.l2(0.01),
-        bias_regularizer = regularizers.l2(0.01)
-    ))
-    model.add(Conv2D(filters=32, kernel_size=1, padding='valid', activation='elu',
-        kernel_regularizer = regularizers.l2(0.01),
-        bias_regularizer = regularizers.l2(0.01)
-    ))
+    model.add(Conv2D(filters=32, kernel_size=1, padding='valid', activation='elu', input_shape=(HEIGHT,WIDTH,5)))
+    model.add(Conv2D(filters=32, kernel_size=3, padding='valid', activation='elu'))
+    model.add(Conv2D(filters=32, kernel_size=1, padding='valid', activation='elu'))
+    model.add(BatchNormalization())
     model.add(MaxPooling2D(pool_size=2))
-    model.add(Conv2D(filters=64, kernel_size=2, padding='valid', activation='elu',
-        kernel_regularizer = regularizers.l2(0.01),
-        bias_regularizer = regularizers.l2(0.01)
-    ))
-    model.add(Conv2D(filters=64, kernel_size=1, padding='valid', activation='elu',
-        kernel_regularizer = regularizers.l2(0.01),
-        bias_regularizer = regularizers.l2(0.01)
-    ))
+    model.add(Conv2D(filters=64, kernel_size=1, padding='valid', activation='elu'))
+    model.add(Conv2D(filters=64, kernel_size=2, padding='valid', activation='elu'))
+    model.add(Conv2D(filters=64, kernel_size=1, padding='valid', activation='elu'))
+    model.add(BatchNormalization())
     model.add(MaxPooling2D(pool_size=2))
-    model.add(Conv2D(filters=128, kernel_size=(2,3), padding='valid', activation='elu',
-        kernel_regularizer = regularizers.l2(0.01),
-        bias_regularizer = regularizers.l2(0.01)
-    ))
-    model.add(Conv2D(filters=128, kernel_size=1, padding='valid', activation='elu',
-        kernel_regularizer = regularizers.l2(0.01),
-        bias_regularizer = regularizers.l2(0.01)
-    ))
+    model.add(Conv2D(filters=128, kernel_size=1, padding='valid', activation='elu'))
+    model.add(Conv2D(filters=128, kernel_size=(2,3), padding='valid', activation='elu'))
+    model.add(Conv2D(filters=128, kernel_size=1, padding='valid', activation='elu'))
+    model.add(BatchNormalization())
     model.add(MaxPooling2D(pool_size=2))
-    model.add(Conv2D(filters=256, kernel_size=2, padding='valid', activation='elu',
-        kernel_regularizer = regularizers.l2(0.01),
-        bias_regularizer = regularizers.l2(0.01)
-    ))
-    model.add(Conv2D(filters=256, kernel_size=1, padding='valid', activation='elu',
-        kernel_regularizer = regularizers.l2(0.01),
-        bias_regularizer = regularizers.l2(0.01)
-    ))
+    model.add(Conv2D(filters=256, kernel_size=1, padding='valid', activation='elu'))
+    model.add(Conv2D(filters=256, kernel_size=2, padding='valid', activation='elu'))
+    model.add(Conv2D(filters=256, kernel_size=1, padding='valid', activation='elu'))
     model.add(MaxPooling2D(pool_size=2))
     model.add(GlobalAveragePooling2D())
+    model.add(BatchNormalization())
     model.add(Dropout(2-PHI))
-    model.add(Dense(256, activation='elu',
-        kernel_regularizer = regularizers.l2(0.01),
-        bias_regularizer = regularizers.l2(0.01),
-    ))
+    model.add(Dense(256, activation='elu'))
+    model.add(BatchNormalization())
     model.add(Dropout(2-PHI))
-    model.add(Dense(256, activation='elu',
-        kernel_regularizer = regularizers.l2(0.01),
-        bias_regularizer = regularizers.l2(0.01),
-    ))
+    model.add(Dense(256, activation='elu'))
+    model.add(BatchNormalization())
     model.add(Dropout(2-PHI))
-    model.add(Dense(256, activation='elu',
-        kernel_regularizer = regularizers.l2(0.01),
-        bias_regularizer = regularizers.l2(0.01),
-        activity_regularizer = regularizers.l2(0.01)
-    ))
+    model.add(Dense(256, activation='elu'))
+    model.add(BatchNormalization())
     model.add(Dropout(2-PHI))
     model.add(Dense(label_count, activation='softmax'))
     return model
