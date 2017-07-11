0020153c9edf70f6005a5ddfc02331c5b7e815a6
luzi82 <luzi82@gmail.com>
2017-07-11 17:28:27 +0800

    classifier_ok.py works

--- classifier_ok.py
+++ classifier_ok.py
@@ -7,7 +7,6 @@ import _util
 
 MODEL_PATH = 'model'
 WEIGHT_FILENAME = 'classifier_ok.hdf5'
-DATA_FILENAME   = 'data.json'
 
 import classifier_ok_model
 
@@ -18,22 +17,28 @@ load_img = _util.load_img
 
 preprocess_img = classifier_ok_model.preprocess_img
 
-class StateClassifier:
+_SCORE_LIST = np.array([i*2+128+.5 for i in range(64)]).astype(np.float)
+
+class OkClassifier:
 
     def __init__(self, model_path):
         weight_path = os.path.join(model_path, WEIGHT_FILENAME)
-        data_path   = os.path.join(model_path, DATA_FILENAME)
-        with open(data_path,'r') as fin:
-            self.data = json.load(fin)
-        self.model = classifier_ok_model.create_model(len(self.data['label_name_list']))
+        self.model = classifier_ok_model.create_model()
         self.model.load_weights(weight_path)
 
-    def get_state(self, img):
+    def get_ok(self, img):
         img = preprocess_img(img)
-        p = self.model.predict(np.expand_dims(img, axis=0))
-        score = np.max(p)
-        label_idx = np.argmax(p)
-        return self.data['label_name_list'][label_idx], score
+        p_list = self.model.predict(np.expand_dims(img, axis=0))
+        score_list = p_list.reshape(HEIGHT)
+        #p_list = (p_list+1)/2
+        p_list = np.maximum(p_list,0)
+        p_list_base = np.sum(p_list)
+        if p_list_base <= 0:
+            return None, score_list
+        p_list = np.reshape(p_list,(1,HEIGHT))
+        p_list = p_list * _SCORE_LIST
+        center = np.sum(p_list) / p_list_base
+        return center, score_list
 
 if __name__ == '__main__':
     import argparse
@@ -44,7 +49,7 @@ if __name__ == '__main__':
     
     img = load_img(args.img_file)
 
-    sc = StateClassifier(MODEL_PATH)
+    sc = OkClassifier(MODEL_PATH)
 
-    label, score = sc.get_state(img)
-    print('{} {}'.format(label, score))
+    center, score_list = sc.get_ok(img)
+    print('{} {}'.format(center, list(score_list)))
