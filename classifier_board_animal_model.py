98ae3dc1888cec71d943d521d894582c73c82df2
Luzi82 <luzi82@gmail.com>
2017-07-12 00:34:02 +0800

    add animal script, unfinished

---
+++ classifier_board_animal_model.py
@@ -0,0 +1,58 @@
+from keras.layers import Conv2D, MaxPooling2D, GlobalAveragePooling2D, GlobalMaxPooling2D
+from keras.layers import Dropout, Flatten, Dense, BatchNormalization, Activation
+from keras.models import Sequential
+from keras import regularizers
+import _util
+import cv2
+import numpy as np
+
+ICON_WIDTH  = 9
+ICON_HEIGHT = 9
+ICON_COUNT = 8
+BOARD_WIDTH = ICON_WIDTH * ICON_COUNT
+BOARD_HEIGHT = ICON_HEIGHT * ICON_COUNT
+ORI_HEIGHT = 1136
+ORI_CROP_Y = 332
+
+INPUT_SHAPE = (ICON_COUNT*ICON_COUNT,ICON_HEIGHT,ICON_WIDTH,5)
+
+_XY_LAYER = _util.xy_layer(ICON_WIDTH, ICON_HEIGHT)
+_XY_LAYER = np.tile(_XY_LAYER,ICON_COUNT*ICON_COUNT)
+_XY_LAYER = np.reshape(_XY_LAYER,(ICON_COUNT*ICON_COUNT,ICON_WIDTH, ICON_HEIGHT,2))
+
+def preprocess_img(img):
+    y0 = int(img.shape[0]*ORI_CROP_Y/ORI_HEIGHT)
+    y1 = y0 + img.shape[1]
+    img = img[y0:y1,:,:]
+    img = cv2.resize(img,dsize=(BOARD_WIDTH,BOARD_HEIGHT),interpolation=cv2.INTER_AREA)
+    img_list = [img[i*ICON_HEIGHT:(i+1)*ICON_HEIGHT,j*ICON_WIDTH:(j+1)*ICON_WIDTH,:]for i in range(ICON_COUNT) for j in range(ICON_COUNT)]
+    img_list = np.array(img_list)
+    img_list = np.append(img_list,_XY_LAYER,axis=3)
+    assert(img_list.shape==INPUT_SHAPE)
+    return img_list
+
+def create_model(label_count):
+    model = Sequential()
+    model.add(Input(shape=INPUT_SHAPE))
+    model.add(Conv2D(filters=32, kernel_size=1, padding='valid', activation='elu'))
+    model.add(Conv2D(filters=32, kernel_size=2, padding='valid', activation='elu'))
+    model.add(Conv2D(filters=32, kernel_size=1, padding='valid', activation='elu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=2))
+    model.add(Conv2D(filters=32, kernel_size=1, padding='valid', activation='elu'))
+    model.add(Conv2D(filters=32, kernel_size=3, padding='valid', activation='elu'))
+    model.add(Conv2D(filters=32, kernel_size=1, padding='valid', activation='elu'))
+    model.add(Conv2D(filters=32, kernel_size=2, padding='valid', activation='elu'))
+    model.add(Conv2D(filters=32, kernel_size=1, padding='valid', activation='elu'))
+    model.add(Flatten())
+    model.add(BatchNormalization())
+    model.add(Dropout(2-PHI))
+    model.add(Dense(32, activation='elu'))
+    model.add(BatchNormalization())
+    model.add(Dropout(2-PHI))
+    model.add(Dense(32, activation='elu', activity_regularizer=regularizers.l1(0.01/32)))
+    model.add(BatchNormalization())
+    model.add(Dropout(2-PHI))
+    model.add(Dense(label_count, kernel_regularizer=regularizers.l1(0.01/(label_count*32))))
+    model.add(Activation('softmax'))
+    return model
---
+++ extract_board_animal.py
@@ -0,0 +1,56 @@
+import _util
+import classifier_board_animal_model
+import cv2
+import glob
+import os
+import shutil
+import numpy as np
+
+ICON_WIDTH  = classifier_board_animal_model.ICON_WIDTH*2
+ICON_HEIGHT = classifier_board_animal_model.ICON_HEIGHT*2
+ICON_COUNT = classifier_board_animal_model.ICON_COUNT
+BOARD_WIDTH = ICON_WIDTH * ICON_COUNT
+BOARD_HEIGHT = ICON_HEIGHT * ICON_COUNT
+ORI_HEIGHT = classifier_board_animal_model.ORI_HEIGHT
+ORI_CROP_Y = classifier_board_animal_model.ORI_CROP_Y
+
+SHAPE = (ICON_COUNT*ICON_COUNT,ICON_HEIGHT,ICON_WIDTH,3)
+
+def preprocess_img(img):
+    y0 = round(img.shape[0]*ORI_CROP_Y/ORI_HEIGHT)
+    y1 = y0 + img.shape[1]
+    img = img[y0:y1,:,:]
+    assert(img.shape==(BOARD_HEIGHT,BOARD_WIDTH,3))
+    img_list = [img[i*ICON_HEIGHT:(i+1)*ICON_HEIGHT,j*ICON_WIDTH:(j+1)*ICON_WIDTH,:]for i in range(ICON_COUNT) for j in range(ICON_COUNT)]
+    img_list = np.array(img_list)
+    print(img_list.shape)
+    assert(img_list.shape==SHAPE)
+    return img_list
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description='extract board animal')
+    parser.add_argument('img_ts', help='img_ts')
+    args = parser.parse_args()
+    
+    fn_list = glob.glob(os.path.join('raw_image','*','*','{}.png'.format(args.img_ts)))
+    assert(len(fn_list)==1)
+    fn = fn_list[0]
+    
+    shutil.rmtree('output',ignore_errors=True)
+    os.makedirs('output')
+
+    img = _util.load_img(fn)
+    img_list = preprocess_img(img)
+
+    for i in range(len(img_list)):
+        fn = '%s-%02d.png'%(args.img_ts,i)
+        fn = os.path.join('output',fn)
+        #print(fn)
+        img = img_list[i]
+        img = ((img+1)*255/2).astype(np.uint8)
+        img = np.array(img,dtype=np.uint8)
+        #print(img.shape)
+        #print(img)
+        cv2.imwrite(fn,img)
